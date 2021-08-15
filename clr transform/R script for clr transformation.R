bb99c81f8ed6baaa7385e61d892efe49a5854007
Kai Yang <chinelite@qq.com>
2021-08-16 01:07:33 +0800

    Update R script for clr transformation.R

--- clr transform/R script for clr transformation.R
+++ clr transform/R script for clr transformation.R
@@ -4,6 +4,7 @@ vOTU<- read.csv(file.choose(), row.names = 1, check.names = 1,header = 1) #first
 #Zero handling
 vOTU.no0 <- cmultRepl(vOTU,output="p-counts")
 write.csv(vOTU.no0,file = 'vOTU.no02.csv')
+
 # clr transform
 vOTU_clr<-clr(t(vOTU.no0) )#first colum=treatment name(CK14,CK7,CK24...) for clr()
 vOTU_clr<- t(vOTU_clr)  
