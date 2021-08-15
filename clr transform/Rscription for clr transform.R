e6787c7b7dd634efd62b1762e85a349ad9f7b680
Kai Yang <chinelite@qq.com>
2021-08-15 22:50:18 +0800

    Add files via upload

---
+++ clr transform/Rscription for clr transform.R
@@ -0,0 +1,11 @@
+library(zCompositions) 
+library(rgr)  
+vOTU<- read.csv(file.choose(), row.names = 1, check.names = 1,header = 1) #first col=vOTU name
+#Zero handling
+vOTU.no0 <- cmultRepl(vOTU,output="p-counts")
+write.csv(vOTU.no0,file = 'C:/Users/chenmolian/Desktop/Data8.8/Figure 1/differece of vOTU/vOTU.no02.csv')
+# clr transform
+vOTU_clr<-clr(t(vOTU.no0) )#first colum=treatment name(CK14,CK7,CK24...) for clr()
+vOTU_clr<- t(vOTU_clr)  
+head(vOTU_clr)
+write.csv(vOTU_clr,file = 'C:/Users/chenmolian/Desktop/Data8.8/Figure 1/differece of vOTU/vOTU.clr2.csv')
